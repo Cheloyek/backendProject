1c62f184c9ea9e3b98a88c8a18685c79a59326db
”vladimir817vk@gmail.com” <vladimir817vk@gmail.com>
2023-04-17 00:10:55 +0300

    add get endpoints

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,43 @@ import express, {Request, Response} from 'express'
 const app = express()
 const port = process.env.PORT || 3000
 
+const products = [{id: 1, title: 'tomato'}, {id: 2, title: 'orange'}]
+const addresses = [{id: 1, value: 'street 1'}, {id: 2, value: 'street 20'}]
+
 app.get('/', (req: Request, res: Response) => {
     let helloMessage = 'Hello :)';
     res.send(helloMessage)
+})
+
+app.get('/products', (req: any, res: Response) => {
+    if (req.query.title) {
+        let searchString = req.query.title.toString();
+        res.send(products.filter(p => p.title.indexOf(searchString) > -1))
+    } else {
+        res.send(products)
+    }
+})
+
+app.get('/products/:id', (req: Request, res: Response) => {
+    const product = products.find(p => p.id === +req.params.id)
+    if (product) {
+        res.send(product)
+    } else {
+        res.send(404)
+    }
+})
+
+app.get('/addresses', (req: Request, res: Response) => {
+    res.send(addresses)
+})
+
+app.get('/addresses/:id', (req: Request, res: Response) => {
+    const idAddress = addresses.find(a => a.id === Number(req.params.id))
+    if (idAddress) {
+        res.send(idAddress)
+    } else {
+        res.send(404)
+    }
 })
 
 app.listen(port, () => {
